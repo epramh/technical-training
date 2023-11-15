5bb37068b4a81d09e82bace06d8badb122ca0f0b
epramh <ep.rew.steh@gmail.com>
2023-11-15 11:11:39 +0300

    First module : test0 and estate

---
+++ test0/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Test0', # The name that will appear in the App list
+    'version': '1.0.0', # Version
+    'summary': 'Test Customization',
+    'sequence': 5,
+    'description': """
+    Long Description of your modules""",
+    'author': 'Odooistic',
+    'company': 'Company Name',
+    'website': 'https://www.website.com',
+    'category': 'Customization',
+    'depends': ['base'], # dependencies
+    'data': [
+        # 'security/ir.model.access.csv',
+
+
+    ],
+    'application': True, # This line says the module is an App, and not a module
+}
