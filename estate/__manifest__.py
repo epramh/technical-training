77619cb6f891c6989ff7c1b6f15b11d8aa4ad3ce
epramh <ep.rew.steh@gmail.com>
2023-11-22 11:02:16 +0300

    Create addons Real Estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Real Estate', # The name that will appear in the App list
+    'version': '1.0.0', # Version
+    'summary': 'Odoo 16 Development',
+    'sequence': 4,
+    'description': """
+    Long Description of your modules""",
+    'author': 'Ravo',
+    'company': 'MGBI',
+    #'website': 'https://www.odoomates.tech',
+    'category': 'Customization',
+    'depends': ['base'], # dependencies
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/menu.xml',
+        #'views/patient.xml',
+        #'security/ir.model.access.csv'
+    ],
+
+    'installable': True,
+    'application': True, # This line says the module is an App, and not a module
+    'license': 'LGPL-3',
+}
