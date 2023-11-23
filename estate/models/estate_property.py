f0ef719cb2528119a585e96e79d0f9b72ae9dd13
epramh <ep.rew.steh@gmail.com>
2023-11-23 12:02:01 +0300

    Add some fields in models of the addons Real Estate

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,4 +5,16 @@ class HospitalPatient(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string='Name', required=True) #Inside '()' for specification
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')], help = "Orientation is used to info the orientation of the garden")
