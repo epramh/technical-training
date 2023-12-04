0c579a5b0d1dc11180fc267019fdbb5f41d1ee4e
epramh <ep.rew.steh@gmail.com>
2023-12-04 14:40:26 +0300

    Compute fields, constrains and validation Error

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import api,fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
@@ -12,7 +13,22 @@ class HospitalPatient(models.Model):
     is_child = fields.Boolean(string="Is Child ?", tracking=True)
     notes = fields.Text(string="Notes")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender", tracking=True)
+    capitalized_name = fields.Char(string='Capitalized Name', compute='_compute_capitalized_name', store=True)
     
+    @api.constrains('is_child', 'age')
+    def _check_child_age(self):
+        for rec in self:
+            if rec.is_child and rec.age == 0:
+                raise ValidationError(_("Age has to be recorded !"))
+
+    @api.depends('name')
+    def _compute_capitalized_name(self):
+        for rec in self:
+            if rec.name:
+                rec.capitalized_name = rec.name.upper()
+            else:
+                rec.capitalized_name = ''
+
     @api.onchange('age')
     def _onchange_age(self):
         if self.age <=10:
