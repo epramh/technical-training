f6ff21c7521bea5caa1077f059ba1abc9dad9414
epramh <ep.rew.steh@gmail.com>
2023-12-08 09:19:47 +0300

    Creating custom_website

---
+++ custom_website/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Custom Odoo Website Qweb Template', # The name that will appear in the App list
+    'version': '1.0.0', # Version
+    'summary': 'Odoo 16 Development',
+    'sequence': 3,
+    'description': """
+    Long Description of your modules""",
+    'author': 'Ravo',
+    'company': 'MGBI',
+    #'website': 'https://www.localhost.com',
+    'category': 'Customization',
+    'depends': [
+        'base',
+        'hr.contract'
+    ], # dependencies
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/menu.xml',
+        # 'views/website.xml'
+    ],
+
+    'installable': True,
+    'application': False, # This line says the module is an App, and not a module
+    'license': 'LGPL-3',
+}
